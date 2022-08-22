6c6e691f7099880b705115d73841919504169c99
chayale5281 <chayale5281@gmail.com>
2022-08-23 02:14:10 +0300

    print ss

--- aaa.java
+++ aaa.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class aaa {
     private String firstName;
     public void PrintName()
     {
         System.out.println(firstName);
+    }
+    public void inputName()
+    {
+        Scanner myObj = new Scanner(System.in);
+        String dd = myObj.nextLine();
     }
 }
